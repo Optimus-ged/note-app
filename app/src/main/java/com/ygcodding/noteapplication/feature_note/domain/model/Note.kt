d25ce7d343804033e76bca02b59e90ec4282447c
Optimus ged <gedeonyalakuhanda@gmail.com>
2024-10-09 18:01:11 +0200

    Note database logic implemented

---
+++ app/src/main/java/com/ygcodding/noteapplication/feature_note/domain/model/Note.kt
@@ -0,0 +1,22 @@
+package com.ygcodding.noteapplication.feature_note.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.ygcodding.noteapplication.ui.theme.BabyBlue
+import com.ygcodding.noteapplication.ui.theme.LightGreen
+import com.ygcodding.noteapplication.ui.theme.RedOrange
+import com.ygcodding.noteapplication.ui.theme.RedPink
+import com.ygcodding.noteapplication.ui.theme.Violet
+
+@Entity
+data class Note(
+    val title: String,
+    val content: String,
+    val timestamp: Long,
+    val color: Int,
+    @PrimaryKey val id: Int? = null
+) {
+    companion object {
+        val noteColors = listOf(RedOrange, LightGreen, Violet, BabyBlue, RedPink)
+    }
+}
