c7be0efc0d9f3919e54c5c5a56627619ad51c99e
Optimus ged <gedeonyalakuhanda@gmail.com>
2024-10-09 23:40:37 +0200

    app module created successfully

---
+++ app/src/main/java/com/ygcodding/noteapplication/NoteApp.kt
@@ -0,0 +1,8 @@
+package com.ygcodding.noteapplication
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NoteApp : Application() {
+}
---
+++ app/src/main/java/com/ygcodding/noteapplication/feature_note/di/AppModule.kt
@@ -0,0 +1,44 @@
+package com.ygcodding.noteapplication.feature_note.di
+
+import android.app.Application
+import androidx.room.Room
+import com.ygcodding.noteapplication.feature_note.data.data_source.NoteDatabase
+import com.ygcodding.noteapplication.feature_note.data.repository.NoteRepositoryImpl
+import com.ygcodding.noteapplication.feature_note.domain.repository.NoteRepository
+import com.ygcodding.noteapplication.feature_note.domain.use_case.DeleteNote
+import com.ygcodding.noteapplication.feature_note.domain.use_case.GetNotes
+import com.ygcodding.noteapplication.feature_note.domain.use_case.NotesUseCases
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providesNotesDatabase(app: Application): NoteDatabase {
+        return Room.databaseBuilder(
+            app,
+            NoteDatabase::class.java, NoteDatabase.DATABASE_NAME,
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesNoteRepository(db: NoteDatabase): NoteRepository {
+        return NoteRepositoryImpl(db.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun providesNoteUseCases(repository: NoteRepository): NotesUseCases {
+        return NotesUseCases(
+            getNotes = GetNotes(repository),
+            deleteNote = DeleteNote(repository)
+        )
+    }
+}
