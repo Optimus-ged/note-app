abb5b75729d52d528ae24a0de130bee68abde125
Optimus ged <gedeonyalakuhanda@gmail.com>
2024-10-11 11:16:24 +0200

    OrderSection composable created

---
+++ app/src/main/java/com/ygcodding/noteapplication/feature_note/presentation/notes/components/OrderSection.kt
@@ -0,0 +1,30 @@
+package com.ygcodding.noteapplication.feature_note.presentation.notes.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.ygcodding.noteapplication.feature_note.domain.util.NoteOrder
+import com.ygcodding.noteapplication.feature_note.domain.util.OrderType
+
+@Composable
+fun OrderSection(
+    modifier: Modifier = Modifier,
+    noteOrder: NoteOrder = NoteOrder.Date(OrderType.Descending),
+    onOrderChange: (NoteOrder) -> Unit
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            DefaultRadioButton(
+                text = "Title",
+                selected = noteOrder is NoteOrder.Title,
+                onSelect = { onOrderChange(NoteOrder.Title(OrderType.Descending)) }
+            )
+        }
+    }
+}
